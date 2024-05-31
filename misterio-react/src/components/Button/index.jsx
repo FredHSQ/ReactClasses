9ec5deef4883d0a108b2a36ac25ca37b92d97a77
Alethos-BK <odahcamperiles@gmail.com>
2024-05-31 16:03:00 -0300

    feat: aplica rest no button também

--- misterio-react/src/components/Button/index.jsx
+++ misterio-react/src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css"
 
-export const Button = ({ onClick, title }) => {
-    return <button style={styles.button} onClick={onClick}>
-        { title }
+export const Button = ({ onClick, title, ...rest }) => {
+    return <button style={styles.button} onClick={onClick} {...rest}>
+        {title}
     </button>
 };
